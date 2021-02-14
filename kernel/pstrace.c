0ca4339c405fed6b6208721ba58d4442bc7d254b
vk2470 <vk2470@columbia.edu>
2021-02-14 13:41:08 -0800

    skeleton for add, with lots of temporary implementations which need to be improved

--- kernel/pstrace.c
+++ kernel/pstrace.c
@@ -6,11 +6,59 @@
 
 #define PSTRACE_BUF_SIZE 500	/* The maximum size of the ring buffer */
 
+int process_tracking_flag = 0;  /*0 by default if process enable is being called for specific 
+				processes, -1 if process enable has been called with pid = -1
+				(signifying that we need to track all processes and 1 if 
+				disable has been called with -1*/
+
+int ring_buf_lock = 0;		/*If lock acquired, 1, else 0. 
+				TODO: FIGURE OUT HOW TO MAKE THIS ATOMIC USING LOCK_PREFIX*/
+
+pid_t enabled_processes[PSTRACE_BUF_SIZE]; 	/*Array that contains list of enabled processes
+						This is only used when 
+						process_tracking_flag != -1
+						Specifically useful when process_tracking_flag = 1
+						and enable call for a specific process comes.
+						NOTE: Can change this datastructure later when we are optimizing*/
+
+pid_t disabled_processes[PSTRACE_BUF_SIZE];	/*Array that contains list of disabled processes
+						This is only used when process_tracking_flag != 1. 
+						Specifically useful when process_tracking_flag = -1
+						and disable call for a specific process comes. In that case
+						we may need to provision more space if number of processes 
+						to disable > PSTRACE_BUF_SIZE.
+						NOTE: Can change this datastructure later when we are optimizing*/
+
+int ring_buf_counter = 0;
+
 /*
+Temporary way of checking if a pid exists. Returns NULL if it does not.
+This might be an overkill because everytime a pid exists, it will return
+the whole taskstruct.
+*/
+static struct task_struct *get_root(int root_pid)
+{
+        if (root_pid == 0)
+                return &init_task;
+
+        return find_task_by_vpid(root_pid);
+}
+
+struct pstrace ring_buf[PSTRACE_BUF_SIZE];
+
+/*TODO: We may need to implement another datastructure to put into ringbuf*/
+
+/*
+Plan of Action:
 1. At the moment we will only track state changes of given pids (or all pids if -1 is sent) after the enable_function is called. This means if a process is in exit zombie when the call was made, we will not keep track of that - when it becomes exit dead, we will be able to track it. Need to check if we should track current state of a process too.
 2. Currently, the plan is to call the pstrace_add from all the necessary places (such as core.c) and pstrace_add will do the check to add into ring buffer.
 3. Datastructure to be used for ring buffer - hashmap or circular linked list.
 4. Need to add the get_root function from hw2 to check if pid exists. Is there another way to do this?
+5. We will have a global flag that contains info for whether we need to 
+	a. track all processes
+	b. track specific processes (default)
+	c. track no process.
+6. Unless we get a pstrace_enable with -1 as pid, we will track only max of 500 processes at any point in time. 
 */
 
 
@@ -72,7 +120,48 @@ SYSCALL_DEFINE1(pstrace_clear,
 
 /* Add a record of the state change into the ring buffer. */
 void pstrace_add(struct task_struct *p){
+	bool to_track = false;
 	
+	if (process_tracking_flag != -1){
+		int i;	
+		for (i = 0; i < PSTRACE_BUF_SIZE; i++){
+			if (p->pid == enabled_processes[i]){
+				to_track = true;
+				break;	
+			}
+		}
+	}
+	else{
+		to_track = true;
+	}
+	if (to_track){
+		struct pstrace tmp_pstrace;
+		int num_chars = 0;
+		while (p->comm[num_chars] != '\0'){
+			tmp_pstrace.comm[num_chars] = p->comm[num_chars];
+			num_chars += 1;
+		}
+		//tmp_pstrace.comm = p->comm;
+		tmp_pstrace.pid = p->pid;
+		if (p->state <= 0){
+			tmp_pstrace.state = p->state;
+		}
+		else{
+			tmp_pstrace.state = p->exit_state;		/*Will this cause problems because 
+									tmp_pstrace.state is of type long
+									whereas p.exit_state is an int.*/		
+		}
+		while(true){						/*TODO: Have a max wait time here
+									for lock to be acquired.*/
+			if (ring_buf_lock == 0){
+				ring_buf_lock = 1;
+				ring_buf[ring_buf_counter % PSTRACE_BUF_SIZE] = tmp_pstrace;
+				ring_buf_counter += 1;
+				ring_buf_lock = 0;
+				break;
+			}
+		}
+	}
 	printk("Inside Add");
 	return;
 }
