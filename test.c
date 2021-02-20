bbc90484be535ca1ba7143bb2b325e625512bd2f
w4118 <vk2470@columbia.edu>
2021-02-20 13:01:09 -0800

    framework for subsequent tests

--- test.c
+++ test.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 {
 	struct pstrace *buf = malloc(500*sizeof(struct pstrace));
 	long *nr = malloc(sizeof(long));
-	*nr = -1;
+	*nr = 1;
 	char *tmp_char = argv[1];
 	pid_t pid;
 	if (tmp_char[0] == '-'){
@@ -33,16 +33,21 @@ int main(int argc, char *argv[])
 	long result = 0;
 	printf("before enable\n");
 	result = syscall(436, pid);
-	//result = syscall(437, pid);
-	//result = syscall(439, pid);
-	printf("after enable before get\n");
 	result = syscall(438, pid ,buf, nr);
 	printf("after get\n");
 	printf(" %ld \n",*nr);
-	for(int i=0;i<500;i++){
-		printf(" buffer state of %d is %ld and comm is %s and pid is %d\n",i,buf[i].state, buf[i].comm, buf[i].pid);
+	result = syscall(438, pid, buf, nr);
+	printf("after get 2\n");
+	printf("%ld\n", *nr);
+	int num_subs_calls = 0;
+	while (num_subs_calls < 1){
+		result = syscall(438, pid, buf, nr);
+		for(int i=0;i<50;i++){
+			printf(" buffer state of %d is %ld and comm is %s and pid is %d\n",i,buf[i].state, buf[i].comm, buf[i].pid);
+		}
+		num_subs_calls += 1;
 	}
-
+	printf("NR %ld", *nr);
 	/*printf("before disable after get\n");
 	result = syscall(437, -1 );
 	printf("after disable\n");
