c7a26672dea8b5368bd7c9c79c410af355383a51
w4118 <vk2470@columbia.edu>
2021-02-22 07:21:40 -0800

    testing -1

---
+++ test_with_arg.c
@@ -0,0 +1,62 @@
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <linux/types.h>
+struct pstrace {
+	char comm[16];/* The name of the process*/
+	pid_t pid;/* The pid of the process*/
+	long state;/* The state of the process*/
+};
+
+
+//end of handlers and structures
+
+int main(int argc, char *argv[])
+{
+	struct pstrace *buf = malloc(500*sizeof(struct pstrace));
+	long *nr = malloc(sizeof(long));
+	*nr = 1;
+	char *tmp_char = argv[1];
+	pid_t pid;
+	if (tmp_char[0] == '-'){
+		pid = atoi(&tmp_char[1]);
+		pid = pid * (-1);
+	}
+	else{
+		pid = atoi(tmp_char);
+	}
+	printf("PID IS %d\n", pid);
+	long result = 0;
+	printf("before enable\n");
+	result = syscall(436, pid);
+	result = syscall(438, pid ,buf, nr);
+	printf("after get\n");
+	printf(" %ld \n",*nr);
+	result = syscall(438, pid, buf, nr);
+	printf("after get 2\n");
+	printf("%ld\n", *nr);
+	int num_subs_calls = 0;
+	while (num_subs_calls < 1){
+		result = syscall(438, pid, buf, nr);
+		for(int i=0;i<50;i++){
+			printf(" buffer state of %d is %ld and comm is %s and pid is %d\n",i,buf[i].state, buf[i].comm, buf[i].pid);
+		}
+		num_subs_calls += 1;
+	}
+	printf("NR %ld", *nr);
+	/*printf("before disable after get\n");
+	result = syscall(437, -1 );
+	printf("after disable\n");
+	printf(" %ld \n",*nr);
+	for(int i=0;i<5;i++){
+		printf(" buffer state of %d is %ld\n",i,buf[i].state);
+		printf(" buffer state of %d is %s\n",i,buf[i].comm);
+	}
+	//result = syscall(439, pid);*/
+
+	return result;
+}
