b5f6b2a9571fd290d4b16d818ea72cb1ecdd363f
cruxcode <ss6146@columbia.edu>
2021-02-22 14:49:35 -0800

    0, 1, 2, 260, 16, 32 all states are appearing

--- six_states.c
+++ six_states.c
@@ -19,13 +19,13 @@ int main(int argc, char **argv)
 	int pstrace_buf_size = 500;
 	int num_processes = 2;
 
-	FILE *fp;
-	char buffer[100];
+	FILE *fp, *outfp;
 
 	struct pstrace *buf = malloc(pstrace_buf_size*num_processes*sizeof(struct pstrace));
 	long *counter = malloc(sizeof(long));
 	int sys_res = 0;
 	*counter = 1;
+	pid_t enabled[num_processes];
 	pid_t pid;
 	for (int i = 0; i < num_processes; i++){	
 		pid = fork();
@@ -49,13 +49,11 @@ int main(int argc, char **argv)
 			}
 		}
 		else{
-			printf("THE PID I AM ENABLING %d\n", pid);
+			enabled[i] = pid;	
 			sys_res = syscall(436, pid);
-			printf("sys_res after enable %d\n", sys_res);	
+			if(sys_res != 0)
+				exit(-1);
 			sleep(5);
-			if (sys_res < 0){
-				printf("SOME ERROR IN ENABLE\n");
-			}
 			kill(pid, SIGCONT);
 			sleep(1);
 			kill(pid, SIGTSTP);
@@ -64,25 +62,26 @@ int main(int argc, char **argv)
 			sleep(0.1);
 			kill(pid, SIGKILL);
 			sleep(0.1);
-			if(i%2 == 0)
-				wait(&sys_res);
+			wait(&sys_res);
 			signal(SIGCHLD, SIG_IGN);
 			sys_res = syscall(438, pid, buf + pstrace_buf_size*i , counter);
 			if (sys_res < 0){
-				printf("SOME ERROR IN GET\n");
+				exit(-1);
 			}	
 			sys_res = syscall(437, pid);
-			printf("PID I AM DISABLING %d\n", pid);
 			if (sys_res < 0){
-				printf("SOME ERROR IN DISABLE\n");
+				exit(-1);
 			}
 		}	
 	}
-	fp = fopen("pstrace.output", "w");
+	
+	outfp = fopen("pstrace.output", "w");	
 	for (int j = 0; j < pstrace_buf_size * num_processes ; j++){
+		if(j % pstrace_buf_size == 0)
+			fprintf(outfp, "pid enabled is %d\n", enabled[j%pstrace_buf_size]);
 		struct pstrace *res = buf + j;	
-		fprintf(fp, "pid %u state %lu comm %s\n", res->pid, res->state, res->comm);
+		fprintf(outfp, "pid %u state %lu comm %s\n", res->pid, res->state, res->comm);
 	}
-	fclose(fp);
+	fclose(outfp);
 	return 0;
 }
