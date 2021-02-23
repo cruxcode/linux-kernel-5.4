3f1665e024c2cff52e47495daee9ade7e5e0b685
George Cherian <gc2920@columbia.edu>
2021-02-22 19:16:44 -0800

    Adding in logic modification for clear

--- kernel/pstrace.c
+++ kernel/pstrace.c
@@ -270,8 +270,24 @@ SYSCALL_DEFINE1(pstrace_clear,
 		for (current_iteration = 0;
 		current_iteration < ring_buffer.current_size;
 		current_iteration++) {
-			if (ring_buffer.buf[i].pid == pid)
-				ring_buffer.buf[ring_buffer.head].pid = -1;
+			if (ring_buffer.buf[i].pid == pid){
+				int j;
+				int k;
+				j=i;
+				for(k=current_iteration;
+				k<ring_buffer.current_size;
+				k++){
+					ring_buffer.buf[j] =
+					ring_buffer.buf
+					[(j +1) % PSTRACE_BUF_SIZE];
+
+					j = (j +1)
+					% PSTRACE_BUF_SIZE;
+				}
+
+				ring_buffer.current_size =
+				ring_buffer.current_size - 1;
+			}
 			i = (i+1)%PSTRACE_BUF_SIZE;
 		}
 	}
